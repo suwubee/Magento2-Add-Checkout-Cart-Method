0e68995c3ee456b3ceb01c3fbd3df5f7f02fe6a6
Ethan Grant <ethangrant93@googlemail.com>
2017-05-30 22:47:30 +0100

    Initial commit

---
+++ Block/Cart/Checkout/Link.php
@@ -0,0 +1,25 @@
+<?php
+namespace Ethangrant\CheckoutMethod\Block\Cart\Checkout;
+
+class Link extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->_isScopePrivate = true;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCheckoutUrl()
+    {
+        return $this->getUrl('checkout');
+    }
+}
